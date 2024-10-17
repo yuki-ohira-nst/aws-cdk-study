f931c90d1d41a5e27724c11f2c44556f6c8c7a4f
yuki-ohira-nst <115784293+yuki-ohira-nst@users.noreply.github.com>
2024-10-17 03:25:18 +0000

    DynamoDBテーブルを作成してデプロイする Fixes #3

--- src/lambda/hello-world.ts
+++ src/lambda/hello-world.ts
@@ -1,11 +1,25 @@
+import { DynamoDBClient, GetItemCommand, GetItemInput } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { Context } from "aws-lambda";
 
 export const handler: any = async (event: any, context: Context) => {
+  const params: GetItemInput = {
+    TableName: 'aws-cdk-study-dev-table8235A42E-1P6XWPTZT386T',
+    Key: {
+      id: { S: '1' },
+    },
+  };
+  const client = DynamoDBDocumentClient.from((new DynamoDBClient({})));
+  const result = await client.send(new GetItemCommand(params));
+
+  console.log('result:', result);
+
   console.log('Hello, world!');
   console.log('event:', event);
   console.log('context:', context);
   return {
     statusCode: 200,
     body: JSON.stringify('Hello, world!'),
+    result,
   };
 };
